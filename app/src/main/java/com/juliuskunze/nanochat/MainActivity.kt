fb13338567b9fe0f4c62cb6bdbc5b3b1421062a7
Julius Kunze <juliuskunze@gmail.com>
2016-03-02 21:59:21 +0100

    cleanup

--- app/src/main/java/com/juliuskunze/nanochat/MainActivity.kt
+++ app/src/main/java/com/juliuskunze/nanochat/MainActivity.kt
@@ -11,6 +11,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    data class ChatMessage(val name: String = "", val text: String = "")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,15 +29,16 @@ class MainActivity : AppCompatActivity() {
             text_edit.setText("")
         }
 
-        val listView = findViewById(android.R.id.list) as ListView
         listView.adapter = object : FirebaseListAdapter<ChatMessage>(this, ChatMessage::class.java,
                 android.R.layout.two_line_list_item, ref) {
             override fun populateView(v: View, model: ChatMessage, position: Int) {
-                (v.findViewById(android.R.id.text1) as TextView).text = model.name
-                (v.findViewById(android.R.id.text2) as TextView).text = model.text
+                v.text1.text = model.name
+                v.text2.text = model.text
             }
         }
     }
 
-    data class ChatMessage(val name: String = "", val text: String = "")
+    val listView: ListView get() = findViewById(android.R.id.list) as ListView
+    val View.text1: TextView get() = findViewById(android.R.id.text1) as TextView
+    val View.text2: TextView get() = findViewById(android.R.id.text2) as TextView
 }
